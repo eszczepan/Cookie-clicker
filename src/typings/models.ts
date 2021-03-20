b0af817e1227eb382b90725477a3dc1281953159
Szczepan <s.szablej1@gmail.com>
2021-03-20 23:30:24 +0100

    Create building object interface

---
+++ src/typings/models.ts
@@ -0,0 +1,10 @@
+export interface IBuilding {
+  title: string;
+  quantity: number;
+  icon: string;
+  baseCost: number;
+  cost: number;
+  baseCps: number;
+  cps: number;
+  upgrade: number;
+}
