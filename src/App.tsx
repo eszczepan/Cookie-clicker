e6c8ba0909cc9cfa20d88763100cf02b8a736227
Szczepan <s.szablej1@gmail.com>
2021-03-22 01:56:58 +0100

    Add statistics page content

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ const App: FC = () => {
       ...prevState,
       cookies: prevState.cookies - cost,
       cookiesPerSecond: cpsCounter(updatedProgress),
+      buildings: prevState.buildings + 1,
     }));
   };
 
--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Card } from "react-bootstrap";
+import { statistics } from "data/statistics";
+import StatsImg1 from "assets/images/statistics/stats-1.png";
+import StatsImg2 from "assets/images/statistics/stats-2.png";
+import StatsImg3 from "assets/images/statistics/stats-3.png";
+import StatsImg4 from "assets/images/statistics/stats-4.png";
+import StatsImg5 from "assets/images/statistics/stats-5.png";
+import StatsImg6 from "assets/images/statistics/stats-6.png";
 import MainTemplate from "components/templates/MainTemplate";
 
 const Statistics = () => {
+  const [progress, setProgress] = useState(
+    JSON.parse(localStorage.getItem("Progress")!) || statistics
+  );
+
   return (
     <MainTemplate>
-      <h1>Statistics</h1>
+      <div className="d-flex justify-content-around flex-wrap mt-5">
+        <Card className="bg-info p-4 mb-3 rounded" style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={StatsImg1} />
+          <Card.Body className="text-center">
+            <Card.Title>{Math.round(progress.cookies)}</Card.Title>
+            <Card.Text>Cookies in bank</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="bg-info p-4 mb-3 rounded" style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={StatsImg2} />
+          <Card.Body className="text-center">
+            <Card.Title>{Math.round(progress.totalCookies)}</Card.Title>
+            <Card.Text>Cookies baked (all time)</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="bg-info p-4 mb-3 rounded" style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={StatsImg3} />
+          <Card.Body className="text-center">
+            <Card.Title>{progress.cookieClicks}</Card.Title>
+            <Card.Text>Cookie clicks</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="bg-info p-4 mb-3 rounded" style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={StatsImg4} />
+          <Card.Body className="text-center">
+            <Card.Title>{progress.level}</Card.Title>
+            <Card.Text>Level reached</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="bg-info p-4 mb-3 rounded" style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={StatsImg5} />
+          <Card.Body className="text-center">
+            <Card.Title>{progress.buildings}</Card.Title>
+            <Card.Text>Buildings amount</Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="bg-info p-4 mb-3 rounded" style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={StatsImg6} />
+          <Card.Body className="text-center">
+            <Card.Title>{progress.cookiesPerSecond}</Card.Title>
+            <Card.Text>Cookies per second</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     </MainTemplate>
   );
 };
