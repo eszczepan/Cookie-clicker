3dfa142171ff8081e07a385e3b15f3bb8de52d40
Szczepan <s.szablej1@gmail.com>
2021-03-23 08:01:36 +0100

    Fix bug with not rounded value

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import MainTemplate from 'components/templates/MainTemplate';
 import CookieCounter from 'components/organisms/CookieCounter/CookieCounter';
 import Store from 'components/organisms/Store/Store';
 import Achievements from 'components/organisms/Achievements/Achievements';
-import { nFormatter } from 'utils/nFormatter';
 
 const App: FC = () => {
   const [progress, setProgress] = useState<IStatistics>(
@@ -139,7 +138,9 @@ const App: FC = () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{nFormatter(progress.cookies).toString()} cookies | Cookie Clicker</title>
+        <title>
+          {nFormatter(Math.round(progress.cookies)).toString()} cookies | Cookie Clicker
+        </title>
         <meta name="description" content="Cookie Clicker game" />
         <meta name="theme-color" content="#666" />
       </Helmet>
